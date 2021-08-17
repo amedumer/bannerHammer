0ae147415c9d313764dedfa7b373f5c4af5af47e
Ahmet Ömer Kayabaşı <69629296+amedumer@users.noreply.github.com>
2021-08-17 03:18:48 +0300

    Add files via upload

---
+++ bannerHammer.py
@@ -0,0 +1,44 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import sys
+
+
+username = sys.argv[1]
+password = sys.argv[2]
+
+
+def goToSite(browser,url):
+    browser.get(url)
+
+def login(browser,username,password):
+    usernamePrompt = browser.find_element_by_xpath("/html/body/div[3]/form/table/tbody/tr[1]/td[2]/input")
+    usernamePrompt.click()
+    usernamePrompt.send_keys(username)
+
+    pwPrompt = browser.find_element_by_xpath("/html/body/div[3]/form/table/tbody/tr[2]/td[2]/input")
+    pwPrompt.click()
+    pwPrompt.send_keys(password)
+
+    pwPrompt.send_keys(Keys.ENTER)
+
+
+def main():
+    browser = webdriver.Firefox()
+
+    goToSite(browser,"https://suis.sabanciuniv.edu/prod/twbkwbis.P_SabanciLogin")
+
+    while browser.title != "User Login":
+        browser.refresh()
+        time.sleep(1)
+
+    login(browser,userpass.giris(),userpass.sifre())
+
+    time.sleep(1)
+
+    goToSite(browser,"https://suis.sabanciuniv.edu/prod/bwskfreg.P_AltPin")
+
+    time.sleep(100)
+    
+
+main()
